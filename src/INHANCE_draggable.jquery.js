5129d2a97b310230a890786b711701cae2ed0069
Myeong Kim <mkim@inhance.com>
2017-10-24 15:23:54 -0700

    prevent click from firing drag handlers

--- src/INHANCE_draggable.jquery.js
+++ src/INHANCE_draggable.jquery.js
@@ -20,7 +20,8 @@
 
 		return this.each(function () {
 			var initDrageePos;
-      var startMousePos;
+			var startMousePos;
+			var isDragStarted = false;
 	    var isMouseDown = false;
 	    var isClick = false;
 	    var me = this;
@@ -33,7 +34,12 @@
 	    		isClick = false;
 	    		$(event.target).trigger('click');
 	    	}
-	    };
+			};
+			
+			var validateClick = function (event) {
+				var delta = Math.sqrt(Math.pow(startMousePos.x - event.pageX, 2) + Math.pow(startMousePos.y - event.pageY, 2));
+				return delta < 8;
+			};
 	    
 	    $(this).on('mousedown.draggable touchstart.draggable', function(event) {
 	    	event.preventDefault();
@@ -44,18 +50,18 @@
         initDrageePos = {x: pos.left, y: pos.top};
 	      startMousePos = {x: event.clientX, y: event.clientY};
 	      isMouseDown = true;
-
-	      if (typeof settings.onDragStart == 'function') settings.onDragStart(me);
 	    });
 	    $(document).on('mouseup.draggable touchend.draggable', function(event) {
 	    	event.preventDefault();
 	      event.stopPropagation();
-	      event = (event.originalEvent.touches && event.originalEvent.touches[0]) || event;
-	      if (isMouseDown) {
-	      	if (typeof settings.onDragEnd == 'function') settings.onDragEnd(me);
-		      var delta = Math.sqrt(Math.pow(startMousePos.x - event.pageX, 2) + Math.pow(startMousePos.y - event.pageY, 2));
-		      if (delta < 8) isClick = true;
-	      	isMouseDown = false;
+				event = (event.originalEvent.touches && event.originalEvent.touches[0]) || event;
+				
+				if (isMouseDown) {
+					var isValidClick = validateClick(event);
+	      	if (!isValidClick && typeof settings.onDragEnd == 'function') settings.onDragEnd(me);
+		      if (isValidClick) isClick = true;
+					isMouseDown = false;
+					isDragStarted = false;
 	      }
 	    });
 	    $(document).on('mousemove.draggable touchmove.draggable', function(event) {
@@ -65,8 +71,15 @@
 	    	if (isMouseDown) {
 	    		var x = event.clientX - (startMousePos.x - initDrageePos.x);
         	var y = event.clientY - (startMousePos.y - initDrageePos.y);
-	      	$(me).offset({left: x, top: y});
-	      	if (typeof settings.onDragging == 'function') settings.onDragging(me);
+					$(me).offset({left: x, top: y});
+					
+					var isValidClick = validateClick(event);
+					if (!isDragStarted && !isValidClick && typeof settings.onDragStart == 'function') {
+						isDragStarted = true;
+						settings.onDragStart(me);
+					}
+					if (!isValidClick && typeof settings.onDragging == 'function') settings.onDragging(me);
+					
 	      }
 	    });
 
