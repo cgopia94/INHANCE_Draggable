73bf502eb0ddb83b6b09d480abb44fd442eb4fdc
Myeong <mkim@inhance.com>
2019-09-16 10:21:48 -0700

    locking axis & container limitation added

--- src/INHANCE_draggable.jquery.js
+++ src/INHANCE_draggable.jquery.js
@@ -3,6 +3,8 @@
 		var settings;
 
 		var defaults = {
+			container : null,
+			lock: null,
 			onDragStart: null,
 			onDragEnd: null,
 			onDragging: null
@@ -35,7 +37,25 @@
 				var delta = Math.sqrt(Math.pow(startMousePos.x - event.pageX, 2) + Math.pow(startMousePos.y - event.pageY, 2));
 				return delta < 8;
 			};
-	    
+
+			var container = document.querySelector(settings.container);
+			if (container) {
+				var myWidth = $(me).outerWidth();
+				var myHeight = $(me).outerHeight();
+				var cBorder = {
+					width: $(container).outerWidth() - $(container).width(),
+					height: $(container).outerHeight() - $(container).height()
+				};
+				var containerInfo = {position: 
+					{
+						x: $(container).offset().left + cBorder.width / 2, 
+						y: $(container).offset().top + cBorder.height / 2}, 
+						width: $(container).width(), 
+						height: $(container).height()
+					};
+			}
+			
+			
 	    $(this).on('mousedown.draggable touchstart.draggable', function(event) {
 	    	event.preventDefault();
 	      event.stopImmediatePropagation();
@@ -64,8 +84,25 @@
 	    	event.stopPropagation();
 	    	event = (event.originalEvent.touches && event.originalEvent.touches[0]) || event;
 	    	if (isMouseDown) {
-	    		var x = event.clientX - (startMousePos.x - initDrageePos.x);
-        	var y = event.clientY - (startMousePos.y - initDrageePos.y);
+	    		var x = settings.lock == 'x' ? initDrageePos.x : event.clientX - (startMousePos.x - initDrageePos.x);
+					var y = settings.lock == 'y' ? initDrageePos.y : event.clientY - (startMousePos.y - initDrageePos.y);
+
+					// container
+					if (container) {
+						if (x == containerInfo.position.x || x < containerInfo.position.x) {
+							x = containerInfo.position.x;
+						}
+						if (x + myWidth == containerInfo.position.x + containerInfo.width || x + myWidth > containerInfo.position.x + containerInfo.width) {
+							x = containerInfo.position.x + containerInfo.width - myWidth;
+						}
+						if (y == containerInfo.position.y || y < containerInfo.position.y) {
+							y = containerInfo.position.y;
+						}
+						if (y + myHeight == containerInfo.position.y + containerInfo.height || y + myHeight > containerInfo.position.y + containerInfo.height) {
+							y = containerInfo.position.y + containerInfo.height - myHeight;
+						}
+					}
+
 					$(me).offset({left: x, top: y});
 					
 					var isValidClick = validateClick(event);
